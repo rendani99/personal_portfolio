48dbbb4305322f3528a85a7bfb68fc0bc4f57e65
rendani99 <rendania460@gmail.com>
2023-06-15 16:47:26 +0200

    delete stange import

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { Home } from '@material-ui/icons'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-//import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Experience from './pages/Experience'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
+          <Route path="/home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
