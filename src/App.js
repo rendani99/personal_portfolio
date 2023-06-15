cc8e222f94775a0595c8e39e8ffaaac024a2b622
rendani99 <rendania460@gmail.com>
2023-06-15 16:41:37 +0200

    create pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Home } from '@material-ui/icons'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
